15647d7493b3577850861d32d86437417d4d415e
Haidar <unityhaidar@gmail.com>
2024-06-07 16:53:34 +0700

    fix: [DK] fix profile picture path

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/image/{filename}', function ($filename) {
+    $path = storage_path('app/profile_pictures/'.$filename);
+
+    if (! File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header('Content-Type', $type);
+
+    return $response;
 });
