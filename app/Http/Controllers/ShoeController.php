c18f8462a910fe5683cffcd4dcdf8997c12954cb
Haidar <unityhaidar@gmail.com>
2024-06-08 16:54:02 +0700

    fix: [DK] fix order and shoe controller

--- app/Http/Controllers/ShoeController.php
+++ app/Http/Controllers/ShoeController.php
@@ -11,14 +11,14 @@ class ShoeController extends Controller
 {
     public function addShoe(AddShoeRequest $request)
     {
-        $validated = $request->validated();
+        $request->validated();
 
 
         $shoe = Shoe::create([
-            'name' => $validated['name'],
-            'addons' => $validated['addons'] ?? null,
-            'notes' => $validated['notes'] ?? null,
-            'price' => $validated['price'],
+            'name' => $request->name,
+            'addons' => $request->addons,
+            'notes' =>  $request-> notes,
+            'price' => $request -> price,
             'order_id' => $request->order_id,
             
         ]);
@@ -31,7 +31,7 @@ class ShoeController extends Controller
 
     public function updateShoe(Request $request)
     {
-        $validated = $request->validate([
+         $request->validate([
             'id' => 'required|integer|exists:shoes,id',
             'name' => 'sometimes|required|string|max:255',
             'addons' => 'sometimes|nullable|string|max:255',
@@ -40,14 +40,14 @@ class ShoeController extends Controller
             'order_id' => 'sometimes|required|integer|exists:orders,id',
         ]);
 
-        $shoe = Shoe::find($validated['id']);
+        $shoe = Shoe::find($request['id']);
         if (!$shoe) {
             return response()->json([
                 'message' => 'Shoe not found',
             ], 404);
         }
 
-        $shoe->update($validated);
+        $shoe->update($request);
 
         return response()->json([
             'message' => 'Shoe updated successfully',
