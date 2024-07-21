e6defa50fd9d97b3f8522c6ed174c7fcfa792bdc
Haidar <unityhaidar@gmail.com>
2024-07-22 02:11:40 +0700

    fix [DK] Average rating

--- database/migrations/2024_06_27_124601_create_reviews_table.php
+++ database/migrations/2024_06_27_124601_create_reviews_table.php
@@ -15,13 +15,13 @@ public function up()
 {
     Schema::create('reviews', function (Blueprint $table) {
         $table->id();
+        $table->enum('order_type', ['regular_clean', 'deep_clean']);
         $table->text('review');
         $table->decimal('rating', 3, 1);
         $table->unsignedBigInteger('user_id');
         $table->unsignedBigInteger('order_id');
         $table->timestamps();
 
-        // Foreign key constraints
         $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
     });
