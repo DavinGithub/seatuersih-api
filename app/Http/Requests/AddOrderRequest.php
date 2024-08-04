35fb30a5f4d8ed041989d66473b90f21207ea2ea
Haidar <unityhaidar@gmail.com>
2024-08-04 19:54:02 +0700

    [DK] fix order status

--- app/Http/Requests/AddOrderRequest.php
+++ app/Http/Requests/AddOrderRequest.php
@@ -30,7 +30,7 @@ class AddOrderRequest extends FormRequest
             'total_price' => 'nullable|numeric',
             'pickup_date' => 'required|date',
             'notes' => 'nullable|string|max:255',
-            'order_status' => 'nullable|string|in:pending,driver on the way to location,shoe being cleaned,completed,decline',
+            'order_status' => 'nullable|string|in:pending,in-progress,completed,decline',
             'kabupaten' => 'required|string|max:255',
             'kecamatan' => 'required|string|max:255',
         ];
