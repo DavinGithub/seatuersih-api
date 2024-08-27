01fcb776e334fdb51a52862342720f9725d43538
Haidar <unityhaidar@gmail.com>
2024-08-27 08:17:18 +0700

    [DK] fix xendit callback token

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Middleware\XenditCallbackToken;
+use App\Http\Middleware\XenditCallbackTokenMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,11 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-     
         $middleware->appendToGroup(
             'xendit-callback',
             [
-                XenditCallbackToken::class,
+                XenditCallbackTokenMiddleware::class,
             ]
         );
     })
