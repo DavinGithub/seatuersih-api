433bbe5ba0b5bc10aa6976fd163de06d118f9a9c
Haidar <unityhaidar@gmail.com>
2024-09-08 13:10:19 +0700

    [DK] fox laundry edit

--- database/seeders/LaundrySeeder.php
+++ database/seeders/LaundrySeeder.php
@@ -18,7 +18,7 @@ class LaundrySeeder extends Seeder
             [
                 'id' => 1,
                 'name' => 'Regular Clean',
-                'Description' => 'Proses pembersihan standar untuk sepatu yang meliputi beberapa langkah dasar untuk menghilangkan kotoran, noda, dan bau yang menempel pada sepatu.',
+                'description' => 'Proses pembersihan standar untuk sepatu yang meliputi beberapa langkah dasar untuk menghilangkan kotoran, noda, dan bau yang menempel pada sepatu.',
                 'price' => '25000', // Menambahkan harga
                 'created_at' => now(),
                 'updated_at' => now(),
@@ -26,7 +26,7 @@ class LaundrySeeder extends Seeder
             [
                 'id' => 2,
                 'name' => 'Deep Clean',
-                'Description' => 'Proses pembersihan standar untuk sepatu yang meliputi beberapa langkah dasar untuk menghilangkan kotoran, noda, dan bau yang menempel pada sepatu.',
+                'description' => 'Proses pembersihan standar untuk sepatu yang meliputi beberapa langkah dasar untuk menghilangkan kotoran, noda, dan bau yang menempel pada sepatu.',
                 'price' => '35000', // Menambahkan harga
                 'created_at' => now(),
                 'updated_at' => now(),
